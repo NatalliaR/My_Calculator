5a73fbe9e14a1ace0aa2dd9bbcb7912617c5af25
Natallia_Ramanchyk <natallia_ramanchyk@epam.com>
2015-10-26 18:55:58 +0300

    First commit

---
+++ src/com/company/Constants.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+
+/**
+ * Created by natalynka on 10/25/15.
+ */
+public class Constants {//создаем класс Constants
+
+    public static final String FIRST_NUMBER = "Enter first number";
+    public static final String SECOND_NUMBER = "Enter second number";
+    public static final String OPERATOR = "Enter '+', '-', '/', '*'";
+    public static final String RESULT = "The result is: ";
+    public static final String ANSWER_YES = "y";
+    public static final String QUESTION_CONTINUE = "Do you want to continue? (y/n)";
+    public static final String ERROR_MESSAGE = "Error: ";
+    public static final String ERROR_UNSUPPORTED_OPERATION = "Unsupported operation";
+    public static final String ERROR_NULL_OPERATION = "Operation must not be null";
+
+}
